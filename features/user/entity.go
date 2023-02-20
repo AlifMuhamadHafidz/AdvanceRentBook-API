686737b065bd47d4582337b70efdeb787ac3dc70
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-02-20 18:40:26 +0700

    chore : validation in helper

--- features/user/entity.go
+++ features/user/entity.go
@@ -1,6 +1,10 @@
 package user
 
-import "github.com/labstack/echo/v4"
+import (
+	"mime/multipart"
+
+	"github.com/labstack/echo/v4"
+)
 
 type Core struct {
 	ID             uint
@@ -17,14 +21,23 @@ type Core struct {
 type UserHandler interface {
 	Register() echo.HandlerFunc
 	Login() echo.HandlerFunc
+	Profile() echo.HandlerFunc
+	Update() echo.HandlerFunc
+	Deactivate() echo.HandlerFunc
 }
 
 type UserService interface {
 	Register(newUser Core) (Core, error)
 	Login(username, password string) (Core, error)
+	Profile(token interface{}) (Core, error)
+	Update(token interface{}, fileData multipart.FileHeader, updateData Core) (Core, error)
+	Deactivate(token interface{}) error
 }
 
 type UserData interface {
-	Register(ewUser Core) (Core, error)
+	Register(newUser Core) (Core, error)
 	Login(username string) (Core, error)
+	Profile(userID uint) (Core, error)
+	Update(userID uint, updateData Core) (Core, error)
+	Deactivate(userID uint) error
 }
---
+++ helper/validation.go
@@ -0,0 +1,52 @@
+package helper
+
+import (
+	"advancerentbook-api/features/user"
+	"errors"
+	"fmt"
+
+	"github.com/go-playground/validator"
+)
+
+type UserValidate struct {
+	Name        string `validate:"required"`
+	Username    string `validate:"required"`
+	BirthOfDate string `validate:"required"`
+	Email       string `validate:"required,email"`
+	Phone       string `validate:"required,numeric"`
+	Address     string `validate:"required"`
+	Password    string `validate:"required,min=3,alphanum"`
+}
+
+func CoreToRegVal(data user.Core) UserValidate {
+	return UserValidate{
+		Name:        data.Name,
+		BirthOfDate: data.BirthOfDate,
+		Username:    data.Username,
+		Email:       data.Email,
+		Phone:       data.Phone,
+		Address:     data.Address,
+		Password:    data.Password,
+	}
+}
+func RegistrationValidate(data user.Core) error {
+	validate := validator.New()
+	val := CoreToRegVal(data)
+	if err := validate.Struct(val); err != nil {
+		for _, e := range err.(validator.ValidationErrors) {
+			vlderror := ""
+			if e.Field() == "Password" && e.Value() != "" {
+				vlderror = fmt.Sprintf("%s is not %s", e.Value(), e.Tag())
+				return errors.New(vlderror)
+			}
+			if e.Value() == "" {
+				vlderror = fmt.Sprintf("%s is %s", e.Field(), e.Tag())
+				return errors.New(vlderror)
+			} else {
+				vlderror = fmt.Sprintf("%s is not %s", e.Value(), e.Tag())
+				return errors.New(vlderror)
+			}
+		}
+	}
+	return nil
+}
