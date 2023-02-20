86854c4313651680439322542843403edbc4b228
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-02-20 12:58:54 +0700

    chore : entity.go

--- features/user/entity.go
+++ features/user/entity.go
@@ -1,5 +1,7 @@
 package user
 
+import "github.com/labstack/echo/v4"
+
 type Core struct {
 	ID             uint
 	Name           string
@@ -10,4 +12,19 @@ type Core struct {
 	Password       string
 	Phone          string
 	Address        string
+}
+
+type UserHandler interface {
+	Register() echo.HandlerFunc
+	Login() echo.HandlerFunc
+}
+
+type UserService interface {
+	Register(newUser Core) (Core, error)
+	Login(username, password string) (Core, error)
+}
+
+type UserData interface {
+	Register(ewUser Core) (Core, error)
+	Login(username string) (Core, error)
 }
