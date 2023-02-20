7ea38d1603ccf0c393cef512334255012de8c44f
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-02-20 23:07:19 +0700

    chore : s3 in helper

--- helper/validation.go
+++ helper/validation.go
@@ -4,9 +4,27 @@ import (
 	"advancerentbook-api/features/user"
 	"errors"
 	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
 
 	"github.com/go-playground/validator/v10"
 )
+
+func TypeFile(test multipart.File) (string, error) {
+	fileByte, _ := io.ReadAll(test)
+	fileType := http.DetectContentType(fileByte)
+	TipenamaFile := ""
+	if fileType == "image/png" {
+		TipenamaFile = ".png"
+	} else {
+		TipenamaFile = ".jpg"
+	}
+	if fileType == "image/png" || fileType == "image/jpeg" || fileType == "image/jpg" {
+		return TipenamaFile, nil
+	}
+	return "", errors.New("file type not match")
+}
 
 type UserValidate struct {
 	Name        string `validate:"required"`
